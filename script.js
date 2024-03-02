13bca369f3a117c52b24a69c70d0ad112587c578
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-03-02 12:02:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
     //Write your code here
-
+	var firstName = document.getElementById('form1').elements['fname'].value;
+    var lastName = document.getElementById('form1').elements['lname'].value;
+    
+    // Concatenate first and last name
+    var fullName = firstName + ' ' + lastName;
+    
+    // Display the full name using alert
+    alert(fullName);
+    
+    // Prevent the form from submitting and refreshing the page
+    return false;
 }
